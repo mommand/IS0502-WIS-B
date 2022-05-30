b5f24d119e1cf3f264ae3cc7eaee52309d3e7e94
Ziaullah <mommand.csf@gmail.com>
2022-05-30 15:16:08 +0430

    last updates

--- showtables.php
+++ showtables.php
@@ -1,34 +1,59 @@
+<style>
+	.pd{
+		padding: 40px;
+	}
+</style>
 <?php
 // include database connection
 include('dbconnection.php');
+// include the asset files (bootstrap files)
 include('assets.php');
-// get database name from url
+// get parameter from url
+
 $dbname = $_GET['dbname'];
-// create show tables query
-$query = "SHOW TABLES FROM ".$dbname;
-// execute show tables query
-$exe_q = $conn->query($query);
-// create a while loop to fetch all the tables within a database
-?>
-<table class="table table-bordered">
-	<tr>
-		<th>ID</th>
-		<th>Table name</th>
-		<th colspan="3">More Actions</th>
-	</tr>
-<?php 
-while ($records = mysqli_fetch_array($exe_q)) {
-	$i++
+
+// use database
+mysqli_select_db($conn, $dbname);
+
+// create mysql query
+
+$q = "SHOW TABLES";
+// execute query
+$exe_query = mysqli_query($conn, $q);
+// check if the query is successfully executed
+if ($exe_query) {
+	// check if the number of rows greater than 0, you should write the condition here to check numbers of affected row in the query.
+	?>
+	<div class="container">
+		<div class="row pd">
+			<h3><?php echo $dbname; ?>'s Tables</h3>
+			<hr>
+		</div>
+	 <table class="table table-bordered">
+	 	<tr>
+	 		<th>ID</th>
+	 		<th>Table Name</th>
+	 		<th colspan="3">More Actions</th>
+	 	</tr>
+	<?php
+	while ($tables = mysqli_fetch_array($exe_query)) {
+		$i++;
 	?>
 		<tr>
 			<td><?php echo $i; ?></td>
-			<td><?php echo $records[0]; ?></td>
-			<td><a href="">Drop Table</a></td>
-			<td><a href="">Describe Table</a></td>
-			<td><a href="">Brows Table</a></td>
+			<td><?php echo $tables[0];?></td>
+			<td><a href="">Drop</a></td>
+			<td><a href="">Describe</a></td>
+			<td><a href="">Browse</a></td>
 		</tr>
 	<?php
+	}
 }
+else{
+	echo "Error";
+}
+
 
 ?>
 </table>
+</div>
